c68cb9b7d1c5b5afd5423ecce273fe1032356e41
ghonth <48602757+ghonth@users.noreply.github.com>
2022-10-21 13:32:06 -0700

    Create nodesAndEdges.py
    Code now does all of the main things that I wanted it to do

--- nodesAndEdges.py
+++ nodesAndEdges.py
@@ -1,8 +1,9 @@
 # import packages
 import pandas as pd
 
-from bokeh.models import ColumnDataSource, CustomJS, HoverTool, TapTool
 from bokeh.plotting import figure, output_file, show
+from bokeh.layouts import row
+from bokeh.models import ColumnDataSource, CustomJS, DataTable, HoverTool, TableColumn, TapTool
 
 # Read in the DataFrame
 data = pd.read_csv("sanitized_data.csv")
@@ -18,6 +19,13 @@ links_list = []
 for key in links.keys():
     links_list.append(links[key])
     
+# Bokeh setup
+output_file("sanitized_data.html")
+p = figure(width=1500, height=500)
+nonsel_color = '#f8faa2'
+sel_color = 'orange'
+nosel_color = '#42b9f5'
+    
 # bokeh ColumnDataSource1 = (x, y) placeholder coordinates for start and end points of each link
 source1 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 
@@ -25,40 +33,21 @@ source1 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 source2 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 
 # bokeh ColumnDataSource3 = associating Col2, Col1, and (x, y) coordinates
-source3 = ColumnDataSource({"x":x,"y":y,'Col2':list(data['Col2']),'Col1':list(data['Col1']), 'Links':links_list})
-
-# Set up Graph and Output file
-output_file("sanitized_example.html")
-p = figure(width=1900, height=700)
+source3 = ColumnDataSource({"x":x,"y":y,'Col2':list(data['Col2']),'Col1':list(data['Col1']), 'Col3':list(data['Col3']), 'Links':links_list, 'color':[nonsel_color]*len(x)})
 
 # Set up line segment and circle frameworks
-sr1 = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.4, line_width=.5, source=source1)
-sr2 = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.4, line_width=.5, source=source2)
-cr = p.circle(x='x', y='y', color='blue', size=5, alpha=1, hover_color='orange', hover_alpha=1.0, source=source3)
+cr = p.circle(x='x', y='y', color=nosel_color, size=6, alpha=1, 
+              hover_color='orange', hover_alpha=1.0, source=source3,
+              selection_color=sel_color,
+              nonselection_fill_color='color', nonselection_fill_alpha=1)
+sr1 = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.7, line_width=.5, source=source1)
+sr2 = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='orange', alpha=0.7, line_width=.5, source=source2)
 
-# Add JS string for HoverTool Callback
-code_for_hover = """
-const links = %s
-const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []}
-const indices = cb_data.index.indices
-for (let i = 0; i < indices.length; i++) {
-    const start = indices[i]
-    for (let j = 0; j < links[start].length; j++) {
-        const end = links[start][j]
-        data['x0'].push(circle.data.x[start])
-        data['y0'].push(circle.data.y[start])
-        data['x1'].push(circle.data.x[end])
-        data['y1'].push(circle.data.y[end])
-    }
-}
-segment.data = data
-""" % links
-
-# Add JS string for Tap segments
-code_for_tap = """
+# JavaScript code for HoverTool CustomJS callback: draw line between hovered-over glyph and all other glyphs on the same Net
+hov_code = """
 const links = %s;
 const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []};
-const indices = cb_obj.indices;
+const indices = cb_data.index.indices;
 for (let i = 0; i < indices.length; i++) {
     const start = indices[i];
     for (let j = 0; j < links[start].length; j++) {
@@ -72,17 +61,47 @@ for (let i = 0; i < indices.length; i++) {
 segment.data = data;
 """ % links
 
+# JavaScript code for TapTool CustomJS callback: make lines remain if you click on a glyph
+tap_code = """
+const links = %s;
+for (let j = 0; j < circle.data.color.length; j++) {
+    circle.data.color[j]='%s';
+}
+const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []};
+const indices = cb_obj.indices;
+for (let i = 0; i < indices.length; i++) {
+    const start = indices[i];
+    for (let j = 0; j < links[start].length; j++) {
+        const end = links[start][j];
+        data['x0'].push(circle.data.x[start]);
+        data['y0'].push(circle.data.y[start]);
+        data['x1'].push(circle.data.x[end]);
+        data['y1'].push(circle.data.y[end]);
+        circle.data.color[end]='%s';
+    }
+}
+circle.change.emit();
+segment.data = data;
+""" % (links, nonsel_color, sel_color)
+
 # Establish Tooltips
 TOOLTIPS = [("Col2","@Col2"), ("Col1","@Col1")]
 
 # Create JavaScript Callbacks
-callback_on_hover = CustomJS(args={'circle': cr.data_source, 'segment': sr1.data_source}, code=code_for_hover)
-callback_on_tap = CustomJS(args={'circle': cr.data_source, 'segment': sr2.data_source}, code=code_for_tap)
+hov_cb = CustomJS(args={'circle': cr.data_source, 'segment': sr1.data_source}, code=hov_code)
+tap_cb = CustomJS(args={'circle': cr.data_source, 'segment': sr2.data_source}, code=tap_code)
 
 # Add tools to the Graph
-p.add_tools(HoverTool(tooltips=TOOLTIPS, callback=callback_on_hover, renderers=[cr]))
+p.add_tools(HoverTool(tooltips=TOOLTIPS, callback=hov_cb, renderers=[cr]))
 p.add_tools(TapTool())
-source3.selected.js_on_change('indices', callback_on_tap)
 
-# Show the graph
-show(p)
+# Call the tap_cb CustomJS callback when you tap
+source3.selected.js_on_change('indices', tap_cb)
+
+# Create DataTable
+columns = [TableColumn(field='Col2', title="Col2"), TableColumn(field='Col1', title="Col1"), TableColumn(field='Col3', title="Col3")]
+data_table = DataTable(source=source3, columns=columns, width=400, height=280, scroll_to_selection=True, selectable=True)
+
+# Order the graph and display it
+layout = row(p, data_table)
+show(layout)
