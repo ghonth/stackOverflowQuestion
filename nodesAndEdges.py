818949ea32fdd1a5b00b77b8bc7c18839fd86fa6
ghonth <48602757+ghonth@users.noreply.github.com>
2022-11-02 09:58:55 -0700

    Get current
    Added DataTable and MiniMap; changed some colors to enhance visibility

--- nodesAndEdges.py
+++ nodesAndEdges.py
@@ -1,4 +1,4 @@
-# import Python packages
+# import packages
 import pandas as pd
 
 from bokeh.plotting import figure, output_file, show
@@ -20,13 +20,13 @@ for key in links.keys():
     links_list.append(links[key])
     
 # Bokeh setup
-output_file("sanitized_data.html")
-p = figure(width=1500, height=500, tools="pan,wheel_zoom,reset")
+output_file("Boroughs_Nets.html")
+p1 = figure(width=1500, height=500, tools="pan,wheel_zoom,reset")
 nonsel_color = '#f8faa2'
 sel_color = 'orange'
 nosel_color = '#42b9f5'
 high_color = 'green'
-    
+
 # bokeh ColumnDataSource1 = (x, y) placeholder coordinates for start and end points of each link
 source1 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 
@@ -34,35 +34,37 @@ source1 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 source2 = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 
 # bokeh ColumnDataSource3 = associating Col2, Col1, and (x, y) coordinates
-source3 = ColumnDataSource({"x":x,"y":y,'Col2':list(data['Col2']),'Col1':list(data['Col1']), 'Col3':list(data['Col3']), 'Links':links_list, 'color':[nonsel_color]*len(x)})
+source3 = ColumnDataSource({"x":x,"y":y,'Col2':list(data['Col2']),
+                            'Col1':list(data['Col1']), 'Col3':list(data['Col3']), 'Links':links_list,
+                            'color':[nonsel_color]*len(x)})
 
 # Set up line segment and circle frameworks
 cr = p1.circle(x='x', y='y', color=nosel_color, size=6, alpha=1, 
               hover_color='orange', hover_alpha=1.0, source=source3,
               selection_color=high_color,
               nonselection_fill_color='color', nonselection_fill_alpha=1)
-sr1 = p1.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.7, line_width=.5, source=source1)
-sr2 = p1.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='orange', alpha=0.7, line_width=.5, source=source2)
+sr1 = p1.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.4, line_width=.5, source=source1)
+sr2 = p1.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='red', alpha=0.4, line_width=.5, source=source2)
 
-# JavaScript code for HoverTool CustomJS callback: draw line between hovered-over glyph and all other glyphs on the same Net
+# Add JS string for HoverTool Callback
 hov_code = """
-const links = %s;
-const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []};
-const indices = cb_data.index.indices;
+const links = %s
+const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []}
+const indices = cb_data.index.indices
 for (let i = 0; i < indices.length; i++) {
-    const start = indices[i];
+    const start = indices[i]
     for (let j = 0; j < links[start].length; j++) {
-        const end = links[start][j];
-        data['x0'].push(circle.data.x[start]);
-        data['y0'].push(circle.data.y[start]);
-        data['x1'].push(circle.data.x[end]);
-        data['y1'].push(circle.data.y[end]);
+        const end = links[start][j]
+        data['x0'].push(circle.data.x[start])
+        data['y0'].push(circle.data.y[start])
+        data['x1'].push(circle.data.x[end])
+        data['y1'].push(circle.data.y[end])
     }
 }
-segment.data = data;
+segment.data = data
 """ % links
 
-# JavaScript code for TapTool CustomJS callback: make lines remain if you click on a glyph
+# Add JS string for Tap segments
 tap_code = """
 const links = %s;
 for (let j = 0; j < circle.data.color.length; j++) {
@@ -92,13 +94,12 @@ mmp_code = """
 """
 
 # Create the box that functions as the current view in the minimap
-box = BoxAnnotation(left=0, right=0, bottom=0, top=0,
-    fill_alpha=0.1, line_color='black', fill_color='black')
+box = BoxAnnotation(left=0, right=0, bottom=0, top=0, fill_alpha=0.1, line_color='black', fill_color='black')
 
 # Establish Tooltips
 TOOLTIPS = [("Col2","@Col2"), ("Col1","@Col1")]
 
-# Create JavaScript Callbacks
+# Create callbacks for HoverTool and TapTool
 hov_cb = CustomJS(args={'circle': cr.data_source, 'segment': sr1.data_source}, code=hov_code)
 tap_cb = CustomJS(args={'circle': cr.data_source, 'segment': sr2.data_source}, code=tap_code)
 xcb = CustomJS(args=dict(box=box), code=mmp_code % ('left', 'right'))
@@ -115,7 +116,7 @@ p1.add_tools(BoxZoomTool(match_aspect=True))
 source3.selected.js_on_change('indices', tap_cb)
 
 # Create DataTable
-columns = [TableColumn(field='Col2', title="Col2"), TableColumn(field='Col1', title="Col1"), TableColumn(field='Col3', title="Col3")]
+columns = [TableColumn(field='Col2', title="Col2"), TableColumn(field='Col3', title="Col3"), TableColumn(field='Col1', title="Col1")]
 data_table = DataTable(source=source3, columns=columns, width=400, height=280, scroll_to_selection=True, selectable=True)
 
 # Dictate view window behavior
